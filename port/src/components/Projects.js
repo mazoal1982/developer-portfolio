88b41f334830ee965ceebaba4dad421f738db7dc
mazoal1982 <aloys.manga@gmail.com>
2024-03-19 18:08:45 -0600

    Added Projects

---
+++ port/src/components/Projects.js
@@ -0,0 +1,32 @@
+import Horiseon from "../assets/Horiseon.png"
+export default function Projects() {
+  
+
+  return (
+<section id="MyWork" className="w3-center">
+  <h2> My Work</h2>
+  <article>
+    <a href="https://mazoal1982.github.io/Coming-Soon-box/">
+      <img height="300px" width="500px" src={Horiseon} />
+    </a>
+  </article>
+  <article>
+    <a href="https://mazoal1982.github.io/generate-random-passwords/">
+      <img height="150px" width="250px" src="https://github.com/mazoal1982/generate-random-passwords/raw/main/Assets/03-javascript-homework-demo.png" />
+    </a>
+    <a href="https://arcane-fortress-44273-29da683b4fc4.herokuapp.com/">
+      <img height="150px" width="250px" src="https://private-user-images.githubusercontent.com/91248776/295306774-fd5667e5-cc77-46bf-84a2-4a8c2c75f160.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTA4OTI0NjcsIm5iZiI6MTcxMDg5MjE2NywicGF0aCI6Ii85MTI0ODc3Ni8yOTUzMDY3NzQtZmQ1NjY3ZTUtY2M3Ny00NmJmLTg0YTItNGE4YzJjNzVmMTYwLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAzMTklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMzE5VDIzNDkyN1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWYxNjJhOTI0YTliODgzODc4NzMxOTkzOWI5NTZlZDYxMWQwMzQyYWVmMmFkZDUzODIyYTk5ZDQ2NGNkMzgxYzQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.MfxY06C1t3K2AUXeUZtQdeyXFkAWDpqbHYU_UDfo3I4" />
+    </a>
+  </article>
+  <article>
+    <a href="https://secret-chamber-35368-7cacc0043d6a.herokuapp.com/">
+      <img height="150px" width="250px" src="https://private-user-images.githubusercontent.com/91248776/314267246-a793c35f-2641-4d4b-8f1a-6389cd98296e.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTA4OTMwMDksIm5iZiI6MTcxMDg5MjcwOSwicGF0aCI6Ii85MTI0ODc3Ni8zMTQyNjcyNDYtYTc5M2MzNWYtMjY0MS00ZDRiLThmMWEtNjM4OWNkOTgyOTZlLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAzMTklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMzE5VDIzNTgyOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWM4ZGU0OTRmMzYxNjI0Mzk3NjQ0ODlhZTVmYzViODNiNzNkZGM2YjJjYjA1ZTJhZDMxZWNjYjcwZTA3ZTE1ZTEmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.ZiniPzR7_V6LbGxuh7e1Ag6IA0J48LJmUNqOy69wyf0" />
+    </a>
+    <a href="https://mazoal1982.github.io/weather-dashboard/">
+      <img height="150px" width="250px" src="https://private-user-images.githubusercontent.com/91248776/314266773-fe6513cd-633a-4eeb-8cf3-536d5cf89b3a.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTA4OTI3OTIsIm5iZiI6MTcxMDg5MjQ5MiwicGF0aCI6Ii85MTI0ODc3Ni8zMTQyNjY3NzMtZmU2NTEzY2QtNjMzYS00ZWViLThjZjMtNTM2ZDVjZjg5YjNhLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAzMTklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMzE5VDIzNTQ1MlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWE3ZjNkOWYzMDgyNGEzNzFjZmI3YzdiNDRiYzQyNDY1MDBjNmYzNDhjMzFlMDA5YjIxNmYzZjFkOTc4YzQxMTMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.4moQTuii4wSczWGRc5hfm3siQo2-ELu1MNeu333qfN4" />
+    </a>
+  </article>
+</section>
+
+  );
+}
